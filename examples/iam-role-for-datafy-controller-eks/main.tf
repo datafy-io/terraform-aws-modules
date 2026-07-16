72d79ac6b9a89281f2e3ed3d70a543dfd24e7135
dimagolomozy <dima.golomozy@gmail.com>
2026-07-16 10:31:08 +0300

    update defaults

--- examples/iam-role-for-datafy-controller-eks/main.tf
+++ examples/iam-role-for-datafy-controller-eks/main.tf
@@ -3,11 +3,10 @@ terraform {
 }
 
 module "irsa" {
-  source = "../../modules/iam-role-for-datafy-controller-eks"
+  source = "datafy-io/modules/aws//modules/iam-role-for-datafy-controller-eks"
 
-  cluster_name                           = "my-eks-cluster"
-  datafy_controller_namespace            = "datafy"
-  datafy_controller_service_account_name = "datafy-controller-sa"
+  cluster_name                = "my-eks-cluster"
+  datafy_controller_namespace = "datafy-agent"
 }
 
 output "iam_role_arn" {
